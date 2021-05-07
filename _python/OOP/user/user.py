4595ab429c03abb08d982853a884ff8f49f9d936
lana yaghi <yaghilana.alaa@gmail.com>
2021-05-07 13:40:30 +0300

    message

---
+++ _python/OOP/user/user.py
@@ -0,0 +1,39 @@
+class User:		
+    def __init__(self, name):
+        self.name = name
+        self.account_balance = 0
+    def make_deposit(self, amount):
+        self.account_balance += amount
+    def make_withdrawal(self, amount):
+        self.account_balance -= amount
+    def display_user_balance(self):
+        print(f"User: {self.name}, Balance: {self.account_balance}")
+    def transfer_money(self, other_user, amount):
+        self.account_balance -= amount
+        other_user.account_balance += amount
+
+
+lana = User("lana")
+lana.make_deposit(1000)
+lana.make_deposit(9720)
+lana.make_deposit(100)
+lana.make_withdrawal(50)
+lana.display_user_balance()
+
+nizam = User("nizam")
+nizam.make_deposit(720)
+nizam.make_deposit(20)
+nizam.make_withdrawal(720)
+nizam.make_withdrawal(700)
+nizam.display_user_balance()
+
+lana = User("Hadeel")
+lana.make_deposit(1000)
+lana.make_deposit(9720)
+lana.make_withdrawal(79)
+lana.make_withdrawal(50)
+lana.display_user_balance()
+
+
+
+
